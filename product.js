3a6a7e292a13452cade44a052dae8bd9d0046e30
Tiffany <tiffanydimaranan@gmail.com>
2021-06-29 23:51:26 +0200

    add button add to cart

--- product.js
+++ product.js
@@ -1,7 +1,7 @@
 const linkPage = window.location.search;
 const urlParams = new URLSearchParams(linkPage);
 const teddyId = urlParams.get('_id');
-console.log(teddyId);
+/*console.log(teddyId);*/
 
 
 async function chosenTeddy() {
@@ -19,7 +19,7 @@ async function chosenTeddy() {
         teddy.classList.add('teddyinfo');
         teddyName.innerText = teddyInfo.name;
         teddyPrice.innerText = teddyInfo.price/100 + '€';
-        teddyImg.setAttribute('src', teddyInfo.imageUrl);
+        teddyImg.src =  teddyInfo.imageUrl;
         teddyDescrip.innerText = teddyInfo.description;
         
         anchor.appendChild(teddy)
@@ -27,31 +27,63 @@ async function chosenTeddy() {
         teddy.appendChild(teddyPrice)
         teddy.appendChild(teddyImg)
         teddy.appendChild(teddyDescrip)
-        
+
+        /*Color options for Teddy*/
         const colorAvailable = teddyInfo.colors;
-          console.log(colorAvailable);
           
           for (color of colorAvailable){
-              console.log(color);
                let ancre = document.getElementById("color");
                let teddyColors = document.createElement('option');
                teddyColors.innerText = color;
                ancre.appendChild(teddyColors)
-          }
+          }  
+
+
+
+        /*BUTTON ADD TO CART*/
+
+       /*create an empty cart*/
+        let array = []
+        if (!localStorage.getItem("panier")){ /*if cart does not exist then...*/
+        localStorage.setItem("panier", JSON.stringify(array)) /*create a cart with empty array*/
+        }
+  
+        /*create add to cart button*/
+        let addToCart = document.getElementById("addtocart")
+        let btnCart = document.createElement("button")
+        btnCart.innerText = "Ajouter au panier"
+        addToCart.appendChild(btnCart)
         
+        /*when button is clicked*/
+        addToCart.onclick = function(){
 
+          let panier = localStorage.getItem("panier") /*get empty cart*/
+          panier =  JSON.parse(panier) /*parse cart*/
 
-        
+          const colorChosen = document.getElementById('color').value; /*take the value of option*/
 
+          /*create object*/
+          const chosenProduct = 
+            {idProduct: teddyInfo._id, 
+            name: teddyInfo.name,
+            color:colorChosen, 
+            price:teddyInfo.price/100,
+            image:teddyInfo.imageUrl}
+          
+          /*console.log(chosenProduct)
+          console.log(panier)*/
+
+          if (panier.length == 0){
+            panier.push(chosenProduct) /*add to panier*/
+            localStorage.setItem("panier" , JSON.stringify(panier))}
+
+            else{ /*cart is not empty, then we can still add another item*/
+              panier.push(chosenProduct) 
+              localStorage.setItem("panier" , JSON.stringify(panier))
+              console.log(panier)}
+        }
+   
     })
 }
 
 chosenTeddy();
-
-
-
-
-
-//const linkPage = window.location.href;
-//const teddyId = linkPage.split('_id=');
-//console.log(teddyId);
